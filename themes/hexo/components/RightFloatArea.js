32439f7f32080f1482c93b22d876b8499e22373a
Delores0217 <delores0217@126.com>
2025-09-29 22:52:22 +0800

    update-test

--- themes/hexo/components/RightFloatArea.js
+++ themes/hexo/components/RightFloatArea.js
@@ -55,7 +55,7 @@ export default function RightFloatArea({ floatSlot }) {
     <div
       className={
         (showFloatButton ? 'opacity-100 ' : 'invisible opacity-0') +
-        '  duration-300 transition-all bottom-12 right-1 fixed justify-end z-20  text-white bg-indigo-500 dark:bg-hexo-black-gray rounded-sm'
+        '  duration-300 transition-all bottom-12 right-1 fixed justify-end z-20  text-white bg-cyan-700 dark:bg-hexo-black-gray rounded-sm'
       }>
       <div
         className={'justify-center flex flex-col items-center cursor-pointer'}>
