869d4947d6e621206e269cd65fd09cc718757cf4
dora <delores0217@126.com>
2026-08-07 16:38:22 +0800

    update ICP & color

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -42,8 +42,9 @@ const CONFIG = {
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
-  HEXO_COLOR_PRIMARY: '#928CEE',
-  HEXO_THEME_COLOR: '#928CEE', // 主题色配置（默认为 #928CEE）
+  HEXO_COLOR_PRIMARY: '#4f627c',
+  HEXO_COLOR_PRIMARY_DARK: '#8899aa',
+  HEXO_THEME_COLOR: '#4f627c', // 主题色配置
 
   /** 文章详情页客户端切换时，主栏显示卡片+转圈占位（无全屏遮罩；已有独立 LoadingCover 的主题无需此项） */
   HEXO_ARTICLE_ROUTE_LOADING: true
